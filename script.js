068dcb8ebcd092cc817d2bfaff9ccb455e908611
Apurba Dutta <apurbadutta2099@gmail.com>
2024-04-16 23:59:05 +0530

    add js

---
+++ script.js
@@ -0,0 +1,10 @@
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const rulesBtn = document.querySelector('.rulesBtn');
+const closeModal = document.querySelector('.close-modal');
+
+// const openModal = function () {};
+
+rulesBtn.addEventListener('click', function () {
+  console.log('clickme');
+});
